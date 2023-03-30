d76cd5a15c60dd38424ecb49b0f77fa992ca5781
Murilo Alexandre Ribeiro <14541596695@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-30 10:37:57 -0300

    aula16

--- aula15/aula15.php
+++ aula15/aula15.php
@@ -1,21 +1,16 @@
-<?php 
-$value = $_GET[''];
-$vetor = [];
-$vetor[] = "yellow";
-$vetor[] = "green";
-$vetor[] = "blue";
-$vetor[] = "purple";
-$vetor[] = "red";
-$vetor[] = "pink";
-$vetor[] = "orange";
-$vetor[] = "grey";
-$vetor[] = "white";
-foreach ($vetor as $key => $value) {
-    echo "<a href='aula15.php?cor=$value'>Mudar para $value</a>"."<br>";
+$cor = ["Yellow","Green","Blue","Pink","Red","Purple","Orange","Grey","White"];
+foreach($cor as $key => $cor){
+    echo "<a href='pagina.php?cor=$cor'> Mudar para $cor </a>" ."<br>";
 }
 
+if(isset($_GET["cor"])){
+    $cor = $_GET["cor"];
+}else{
+    $cor  ="white";
+}
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,9 +18,20 @@ foreach ($vetor as $key => $value) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<style>
+    body{
+        background-color:<?php echo $cor ?>;
+    }
+</style>
+
 </head>
 <body>
+<?php
+$cor = ["Yellow","Green","Blue","Pink","Red","Purple","Orange","Grey","White"];
+foreach($cor as $key => $cor){
+    echo "<a href='pagina.php?cor=$cor'> Mudar para $cor </a>" ."<br>";
+}
+?>
 
-    
 </body>
 </html>
