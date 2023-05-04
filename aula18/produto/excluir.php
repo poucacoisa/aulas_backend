1bc35d7ec25b2956aa07c97196a6fa4d8d6fea50
Murilo Alexandre Ribeiro <14541596695@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-04 10:37:30 -0300

    aula183

--- aula18/produto/excluir.php
+++ aula18/produto/excluir.php
@@ -1,16 +1,16 @@
 <?php
  require_once "../conexao.php";
- 
- $nome = $_POST["nome"];
- $descricao = $_POST["descricao"];
- $preco = $_POST["preco"];
+ //verifica se foi enviada o param pela url
+ if(isset($_GET['id'])){
+$id = $_GET["id"];
  //string com o comendo sql para ser executoado no db
- $sql = "INSERT INTO `produto` (`nome`, `descrição`, `preco`, `foto`) VALUES (?,?, ?, ?);";
+ $sql = "DELETE FROM `produto` WHERE  `idproduto`= ?;";
  echo $sql;
  //prepara o slq para ser executado no banco de dados
  $comando=$conexao->prepare($sql);
  //adiciona os valores nos parametros
- $comando->bind_param("ssds", $nome, $descricao, $preco, $foto);
+ $comando->bind_param("i", $id);
  //executa o sql - comando no banco de dados
  $comando->execute();
  //header("location: form.php");
+}
